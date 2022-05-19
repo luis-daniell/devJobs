c8c99b37984ee6ab9913cacf2675697041dc18e5
Daniel <luisdanielcastro16@gmail.com>
2022-05-19 14:57:13 -0500

    Ultimos cambios de hoy

--- database/migrations/2022_05_18_105028_create_vacantes_table.php
+++ database/migrations/2022_05_18_105028_create_vacantes_table.php
@@ -20,13 +20,11 @@ class CreateVacantesTable extends Migration
             $table->timestamps();
         });
 
-
         Schema::create('experiencias', function (Blueprint $table) {
             $table->id();
             $table->string('nombre');
             $table->timestamps();
         });
-
 
         Schema::create('ubicacions', function (Blueprint $table) {
             $table->id();
@@ -34,15 +32,11 @@ class CreateVacantesTable extends Migration
             $table->timestamps();
         });
 
-
-
         Schema::create('salarios', function (Blueprint $table) {
             $table->id();
             $table->string('nombre');
             $table->timestamps();
         });
-
-
 
         Schema::create('vacantes', function (Blueprint $table) {
             $table->id();
