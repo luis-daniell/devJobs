7081c6a17e5dcdeff9c8e28225dcda7a9938b3d2
Daniel <luisdanielcastro16@gmail.com>
2022-05-20 00:17:20 -0500

    bajada de archivos

--- public/js/app.js
+++ public/js/app.js
@@ -49931,8 +49931,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! D:\Escritorio\devJobs\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! D:\Escritorio\devJobs\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! C:\Users\luisd\Desktop\Mis Cursos\Curso Laravel\devJobs\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\Users\luisd\Desktop\Mis Cursos\Curso Laravel\devJobs\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
